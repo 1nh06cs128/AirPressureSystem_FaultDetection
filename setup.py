eb2f11ed20d4d7c8289cb9798a767b5ed3a0eecb
1nh06cs128 <sb.1nh06cs128@gmail.com>
2022-11-27 15:45:13 +0530

    updated setup.py file

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-import os
-import platform
-import re
-import sys
-import warnings
+from setuptools import find_packages, setup
+from typing import List
 
+REQUIREMENTS_FILE_NAME="requirements.txt"
+HYPEN_E_DOT="-e ."
 
-from setuptool import find_packages, setup
-
-def get_requirements():
-    pass
+def get_requirements()->List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
+        requirement_list = requirements_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPEN_E_DOT)
+    return requirement_list
 
 setup(
     name="sensor",
     version="0.0.1",
     author="Saurav Bhalotia",
-    
+    author_email="sb.1nh06cs128@gmail.com",
     # if a folder contains a file __init__.py file 
     # that implies that particular folder is a package / module / etc.
-    packages= find_packages(),
-    
+    packages= find_packages(),    
     install_requires=get_requirements(),
 )
